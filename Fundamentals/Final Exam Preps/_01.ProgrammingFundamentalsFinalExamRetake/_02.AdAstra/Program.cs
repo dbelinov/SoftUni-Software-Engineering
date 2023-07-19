776c2e49d26453f7c7502de74ef56d79f8996975
dbelinov <ddbelinov@gmail.com>
2023-07-19 15:50:34 +0300

    New Solution, Projects and Sorting
    Added solution "01. Programming Fundamentals Final Exam Retake" and all projects to it, as well as sorted all exam preps in folders

---
+++ Fundamentals/Final Exam Preps/_01.ProgrammingFundamentalsFinalExamRetake/_02.AdAstra/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+/*
+#Bread#19/03/21#4000#|Invalid|03/03.20||Apples|08/10/20|200||Carrots|06/08/20|500||Not right|6.8.20|5|
+*/
+
+namespace _02.AdAstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"([#|])(?<Name>[A-Z-a-z ]+)\1(?<ExpirationDate>\d{2}/\d{2}/\d{2})\1(?<Calories>\d+)\1";
+            var matches = Regex.Matches(input, pattern);
+            int totalCalories = 0;
+            foreach (Match match in matches)
+            {
+                totalCalories += int.Parse(match.Groups["Calories"].Value);
+            }
+
+            int daysToSurvive = totalCalories / 2000;
+
+            Console.WriteLine($"You have food to last you for: {daysToSurvive} days!");
+            foreach (Match match in matches)
+            {
+                string name = match.Groups["Name"].Value;
+                string expiry = match.Groups["ExpirationDate"].Value;
+                int calories = int.Parse(match.Groups["Calories"].Value);
+
+                Console.WriteLine($"Item: {name}, Best before: {expiry}, Nutrition: {calories}");
+            }
+        }
+    }
+}
---
+++ Fundamentals/Final Exam Preps/_01.ProgrammingFundamentalsFinalExamRetake/_03.ThePianist/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+/*
+3
+Fur Elise|Beethoven|A Minor
+Moonlight Sonata|Beethoven|C# Minor
+Clair de Lune|Debussy|C# Minor
+Add|Sonata No.2|Chopin|B Minor
+Add|Hungarian Rhapsody No.2|Liszt|C# Minor
+Add|Fur Elise|Beethoven|C# Minor
+Remove|Clair de Lune
+ChangeKey|Moonlight Sonata|C# Major
+Stop
+*/
+
+namespace _03.ThePianist
+{
+    class Piece
+    {
+        public Piece(string name, string composer, string key)
+        {
+            Name = name;
+            Composer = composer;
+            Key = key;
+        }
+
+        public string Name { get; set; }
+        public string Composer { get; set; }
+        public string Key { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} -> Composer: {Composer}, Key: {Key}";
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            int numberOfPieces = int.Parse(Console.ReadLine());
+            List<Piece> pieces = new List<Piece>();
+            for (int i = 0; i < numberOfPieces; i++)
+            {
+                string input = Console.ReadLine();
+                string[] tokens = input.Split('|');
+                string name = tokens[0];
+                string composer = tokens[1];
+                string key = tokens[2];
+                Piece piece = new Piece(name, composer, key);
+                pieces.Add(piece);
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "Stop")
+            {
+                string[] arguments = command.Split('|');
+                switch (arguments[0])
+                {
+                    case "Add":
+                        string name = arguments[1];
+                        string composer = arguments[2];
+                        string key = arguments[3];
+                        Piece foundPiece = pieces.FirstOrDefault(x => x.Name == name);
+                        if (foundPiece == null)
+                        {
+                            Piece piece = new Piece(name, composer, key);
+                            pieces.Add(piece);
+                            Console.WriteLine($"{name} by {composer} in {key} added to the collection!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{name} is already in the collection!");
+                        }
+                        break;
+                    case "Remove":
+                        string nameToRemove = arguments[1];
+                        Piece pieceExists = pieces.FirstOrDefault(x => x.Name == nameToRemove);
+                        if (pieceExists != null)
+                        {
+                            pieces.Remove(pieceExists);
+                            Console.WriteLine($"Successfully removed {nameToRemove}!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Invalid operation! {nameToRemove} does not exist in the collection.");
+                        }
+                        break;
+                    case "ChangeKey":
+                        string nameToEdit = arguments[1];
+                        string newKey = arguments[2];
+                        Piece pieceIsInCollection = pieces.FirstOrDefault(x => x.Name == nameToEdit);
+                        if (pieceIsInCollection != null)
+                        {
+                            pieceIsInCollection.Key = newKey;
+                            Console.WriteLine($"Changed the key of {nameToEdit} to {newKey}!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Invalid operation! {nameToEdit} does not exist in the collection.");
+                        }
+                        break;
+                }
+            }
+
+            foreach (Piece piece in pieces)
+            {
+                Console.WriteLine(piece);
+            }
+        }
+    }
+}
