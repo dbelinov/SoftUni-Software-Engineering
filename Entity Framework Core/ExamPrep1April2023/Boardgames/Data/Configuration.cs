d0ee63037694708c848ee5f50e847a76327f97c6
dbelinov <ddbelinov@gmail.com>
2024-07-30 13:38:13 +0300

    Added Project
    Progress towards Exam Preparation 1 April 2023

---
+++ Entity Framework Core/ExamPrep1April2023/Boardgames/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Boardgames.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=.;Database=Boardgames;User Id=sa;Password=VeryStr0ngP@ssw0rd;";
+    }
+}
